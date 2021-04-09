5f43e0ab80dbe4a21bc35ce34300f6522834e5f1
unknown <brezo.al@gmail.com>
2021-04-09 10:19:38 +0200

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './counter/counter.component';
+import { HeroComponent } from './heros/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-counter',
+  template: `
+    <h1>{{ title }}</h1>
+    <h3>La base es: <strong>{{ base }}</strong></h3>
+
+    <button (click)="operation(+base)">+{{base}}</button>
+    <span>{{ number }}</span>
+    <button (click)="operation(-base)">-{{base}}</button>
+  `
+})
+
+export class ContadorComponent {
+  title: string = 'Contador APP';
+  number: number = 0;
+  base: number = 5;
+
+  operation(valor: number) {
+    this.number += valor;
+  }
+}
